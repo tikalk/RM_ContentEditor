e0fc77806799785670a35fa9c43250c8c1d43517
Alon Valadji <alon@ronin.co.il>
2017-07-10 17:02:11 +0300

    :recycle: refactor to separate components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import './index.css';
 import App from './App';
 import store from './store';
 import { initPubSub } from './iframePubSub';;
